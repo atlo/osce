fc2339786d1599f5019da1f7a6de6eaeebb1735d
László Horváth <bedafoltyn@gmail.com>
2019-08-12 18:00:10 +0200

    slider calculation and styling fixed

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const container = document.querySelector('#atlo')
 const backButton = document.querySelector('.back-button')
 const nextButton = document.querySelector('.next-button')
 const tooltip = document.querySelector('.tooltip')
+const tooltipTriangle = document.querySelector('.tooltip:before')
 const tooltipText = document.querySelector('.tooltip-text')
 const textTitle = document.querySelector('.content-text h2')
 const textParagraph = document.querySelector('.content-text p')
@@ -17,46 +18,212 @@ let currentPage = 0
 const data = [
   {
     id: 1,
-    title: 'Lorem ipsum #1',
-    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
-    video: 'Valentina_Andrasek.mp4',
+    title: 'Perpetrators',
+    description: 'For women directly affected by conflict, the perpetrator of these assaults is much more likely to be someone other than an intimate partner compared to women who are not directly conflict-affected (78% versus 46%). (Page 6)',
     left: {
-      percentage: 24,
-      tooltip: {
-        position: 17,
-        text: '<p>Men are allowed to do everything. They can go out whenever they want, they can just stay in betting shops and drink alcohol and smoke cigarettes.</p><p>Female, aged 36-55, urban, Albania</p>'
-      }
+      percentage: 78
     },
     right: {
-      percentage: 13
+      percentage: 46
     }
   }, {
     id: 2,
-    title: 'Lorem ipsum #2',
-    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
-    video: 'Marija_Babovic_pt_1.mp4',
+    title: 'Perpetrators',
+    description: 'Serious incidents of non-partner violence connected to conflict are much more likely to be at the hands of multiple perpetrators, with the majority of incidents (55%) perpetrated by three or more people. This is eleven times more than the average of 5% across all victims of non-partner violence. (page 7)',
     left: {
-      percentage: 33
+      percentage: 55
     },
     right: {
-      percentage: 43,
-      tooltip: {
-        position: 21,
-        text: '<p>Men are allowed to do everything. They can go out whenever they want, they can just stay in betting shops and drink alcohol and smoke cigarettes.</p><p>Female, aged 36-55, urban, Albania</p>'
-      }
+      percentage: 5
     }
   }, {
     id: 3,
-    title: 'Lorem ipsum #3',
-    description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
-    video: 'Marija_Babovic_pt2.mp4',
+    title: 'Perpetrators',
+    description: 'Perpetrators of the most serious incident of intimate partner or non-partner violence connected with conflict are not more likely to be drunk at the time of this incident, but victims of these incidents are more likely to say that the perpetrator was either under the influence of drugs (8% compared with 1% on average), or under the influence of both drugs and alcohol (15% compared with 2% on average). (page 7)',
     left: {
-      percentage: 17
+      percentage: 15
     },
     right: {
-      percentage: 23
+      percentage: 2
     }
   }, {
+    id: 4,
+    title: 'Severity of violance in conflict',
+    description: 'being pushed or shoved',
+    left: {
+      percentage: 34
+    },
+    right: {
+      percentage: 40
+    }
+  }, {
+    id: 5,
+    title: 'Severity of violance in conflict',
+    description: 'having their head beaten against something',
+    left: {
+      percentage: 7
+    },
+    right: {
+      percentage: 4
+    }
+  }, {
+    id: 6,
+    title: 'Severity of violance in conflict',
+    description: 'being raped',
+    left: {
+      percentage: 8
+    },
+    right: {
+      percentage: 6
+    }
+  }, {
+    id: 7,
+    title: 'Severity of violance in conflict',
+    description: 'being forced to partake in some form of sexual activity when they did not want to',
+    left: {
+      percentage: 6
+    },
+    right: {
+      percentage: 4
+    }
+  }, {
+    id: 8,
+    title: 'Experiences of refugees, IDPS and returnees (use also quotes)',
+    description: 'refugees or displaced / being cut or stabbed, or shot',
+    left: {
+      percentage: 17,
+      tooltip: {
+        position: 13,
+        text: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p><p>Female, aged 36-55, urban, Albania</p>'
+      }
+    },
+    right: {
+      percentage: 2
+    }
+  }, {
+    id: 9,
+    title: 'Experiences of refugees, IDPS and returnees (use also quotes)',
+    description: 'returnees / being raped',
+    left: {
+      percentage: 21
+    },
+    right: {
+      percentage: 6,
+      tooltip: {
+        position: 3,
+        text: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p><p>Female, aged 36-55, urban, Albania</p>'
+      }
+    }
+  }, {
+    id: 10,
+    title: 'Health effects of violance against women in clonflict',
+    description: 'suffering from a psychological impact',
+    left: {
+      percentage: 85
+    },
+    right: {
+      percentage: 81
+    }
+  }, {
+    id: 11,
+    title: 'Health effects of violance against women in clonflict',
+    description: 'depression',
+    left: {
+      percentage: 43
+    },
+    right: {
+      percentage: 28
+    }
+  }, {
+    id: 12,
+    title: 'Health effects of violance against women in clonflict',
+    description: 'anxiety',
+    left: {
+      percentage: 50
+    },
+    right: {
+      percentage: 39
+    }
+  }, {
+    id: 13,
+    title: 'Health effects of violance against women in clonflict',
+    description: 'loss of self-confidence',
+    left: {
+      percentage: 37
+    },
+    right: {
+      percentage: 18
+    }
+  }, {
+    id: 14,
+    title: 'Barriers to reporting, experiences of reporting and satisfaction with services',
+    description: 'However, where the most serious incident of violence was connected to conflict, victims of all perpetrator types are less likely than women overall to have reported the incident to the police (13% compared to 18% of all serious incidents). Page 8',
+    left: {
+      percentage: 13,
+      tooltip: {
+        position: 9,
+        text: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p><p>Female, aged 36-55, urban, Albania</p>'
+      }
+    },
+    right: {
+      percentage: 18
+    }
+  }, {
+    id: 15,
+    title: 'Barriers to reporting, experiences of reporting and satisfaction with services',
+    description: 'the police would not do anything',
+    left: {
+      percentage: 7
+    },
+    right: {
+      percentage: 16,
+      tooltip: {
+        position: 14,
+        text: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p><p>Female, aged 36-55, urban, Albania</p>'
+      }
+    }
+  }, {
+    id: 16,
+    title: 'Barriers to reporting, experiences of reporting and satisfaction with services',
+    description: 'not wanting anyone to know about it',
+    left: {
+      percentage: 11
+    },
+    right: {
+      percentage: 18
+    }
+  }, {
+    id: 17,
+    title: 'Barriers to reporting, experiences of reporting and satisfaction with services',
+    description: 'Women who are directly affected by conflict are somewhat less likely to have spoken to anyone other than the police and other institutions about their most serious incident of violence, with 59% having done so, compared to 65% of non-conflict affected women. ',
+    left: {
+      percentage: 59
+    },
+    right: {
+      percentage: 65
+    }
+  }, {
+    id: 18,
+    title: 'Partner was involved in conflict',
+    description: 'intimate partner physical and/or sexual violence since the age of 15',
+    left: {
+      percentage: 30
+    },
+    right: {
+      percentage: 22
+    }
+  }, {
+    id: 19,
+    title: 'Partner was involved in conflict',
+    description: 'Women whose current partner suffered from at least one psychological impact /  physical and/or sexual violence at their hands since the age of 15 ',
+    left: {
+      percentage: 48
+    },
+    right: {
+      percentage: 15
+    }
+  }
+  /* {
     id: 3,
     title: 'Lorem ipsum #4',
     description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.',
@@ -71,7 +238,7 @@ const data = [
     right: {
       percentage: 3
     }
-  }
+  } */
 ]
 
 function range (length) {
@@ -127,14 +294,26 @@ function setHighlightedElements (group, {columns, rows}) {
 }
 
 function setToolTip (group, column, row) {
-  document
-    .querySelector(`.${group} li:nth-of-type(${column}) div:nth-of-type(${5 - row})`)
-    .classList.add('action')
+  console.log({group, column, row})
+  const activeIcon = document.querySelector(`.${group} li:nth-of-type(${column}) div:nth-of-type(${5 - row})`)
+  
+  activeIcon.classList.add('action')
+  activeIcon.addEventListener('click', toggleTooltip)
+  activeIcon.addEventListener('mouseenter', showTooltup)
+  activeIcon.addEventListener('mouseleave', hideTooltup)
+  tooltip.style.top = `${(5 - row) * 20 + 5}%`
+
+  if (group === 'left') {
+    tooltip.classList.remove('right')
+    tooltip.style.left = `${41.5 - (20 - column) * 2.5}%`
+  } else {
+    tooltip.classList.add('right')
+    tooltip.style.left = `${0.5 + (column) * 2.5}%`
+  }
 }
 
 function paginate (isNext) {
   tooltip.classList.remove('active')
-  tooltipText.classList.remove('active')
 
   Array
     .from(document.querySelectorAll('.slider div.action'))
@@ -156,9 +335,9 @@ function paginate (isNext) {
   setHighlightedElements('right', calculateRowsAndColumns(right.percentage))
 
   leftNumber.innerHTML = `${selected.left.percentage}%`
-  leftNumber.style.left = `${33 - selected.left.percentage / 2}%`
+  /* leftNumber.style.left = `${40 - selected.left.percentage / 2}%` */
   rightNumber.innerHTML = `${selected.right.percentage}%`
-  rightNumber.style.left = `${54 + selected.right.percentage / 2}%`
+  rightNumber.style.left = `calc(100% - ${rightNumber.offsetWidth}px)`
 
   video.innerHTML = `
   <video controls>
@@ -166,47 +345,40 @@ function paginate (isNext) {
   </video>`
 
   if (left.tooltip) {
+    console.log(left.tooltip)
     const {columns, rows} = calculateRowsAndColumns(left.tooltip.position)
     setToolTip('left', 20 - columns, rows - 1)
 
-    tooltip.classList.add('active')
-    tooltipText.innerHTML = tooltip.text
+    tooltipText.innerHTML = left.tooltip.text
   }
 
   if (right.tooltip) {
+    console.log(right.tooltip)
     const {columns, rows} = calculateRowsAndColumns(right.tooltip.position)
     setToolTip('right', columns + 1, rows - 1)
 
-    tooltip.classList.add('active')
-    tooltipText.innerHTML = tooltip.text
+    tooltipText.innerHTML = right.tooltip.text
   }
 }
 
 function toggleTooltip (event) {
   event.stopPropagation()
 
-  tooltipButton.classList.toggle('active')
-  tooltipText.classList.toggle('active')
+  tooltip.classList.toggle('active')
 }
 
 function showTooltup (event) {
   event.stopPropagation()
 
-  tooltipButton.classList.add('active')
-  tooltipText.classList.add('active')
+  tooltip.classList.add('active')
 }
 
 function hideTooltup (event) {
   event.stopPropagation()
 
-  tooltipButton.classList.remove('active')
-  tooltipText.classList.remove('active')
+  tooltip.classList.remove('active')
 }
 
 paginate(false)
 backButton.addEventListener('click', e => paginate(false))
 nextButton.addEventListener('click', e => paginate(true))
-
-/* tooltipButton.addEventListener('click', toggleTooltip)
-tooltipButton.addEventListener('mouseenter', showTooltup)
-tooltipButton.addEventListener('mouseleave', hideTooltup) */
